aaf3bf48a061876941c1021aa3bcee5c632f4529
MonsterMannen <viktorsvensson94@gmail.com>
2023-08-01 00:34:56 +0200

    update

--- background.js
+++ background.js
@@ -13,12 +13,12 @@ See https://developer.chrome.com/blog/mv2-transition/ for more details.
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
-            if (details.requestHeaders[i].name === 'Accept') {
-                details.requestHeaders[i].value = 'image/avif,image/webp,*/*';
+            if (details.requestHeaders[i].name === "Accept") {
+                details.requestHeaders[i].value = "image/avif,image/webp,*/*";
             }
         }
         return { requestHeaders: details.requestHeaders };
     },
-    { urls: ["<all_urls>"] },
+    { urls: ["*://i.redd.it/*", "*://external-preview.redd.it/*", "*://preview.redd.it/*"] },
     ["blocking", "requestHeaders"]
 );
