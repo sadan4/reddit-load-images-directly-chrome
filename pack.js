48fd8ddcb208bf387cc66912d504f64ab1bfdcf0
sadan <117494111+sadan4@users.noreply.github.com>
2024-06-02 01:20:25 -0400

    fix file ext

--- pack.js
+++ pack.js
@@ -13,6 +13,6 @@ const c = new crx({
 
 c.load( path.resolve(__dirname)).then(c => c.pack()).then(cb => {
     const xml = c.generateUpdateXML()
-    fs.writeFileSync("./reddit-load-images-directly-chrome", cb);
+    fs.writeFileSync("./reddit-load-images-directly-chrome.crx", cb);
     fs.writeFileSync("./update.xml", xml);
 }).catch(e => console.error(e))
