7020ad42832284a1cd32bf5803df22cdd40355b2
sadan <117494111+sadan4@users.noreply.github.com>
2024-06-02 00:00:22 -0400

    pls work

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd $GITHUB_WORKSPACE
-npm i
-TEMP=$(mktemp)
-printf "%s" "$KEY" > $TEMP
-npx crx pack -o reddit-load-images-directly-chrome.crx -p $TEMP
---
+++ pack.js
@@ -0,0 +1,18 @@
+const fs = require("fs")
+const path = require("path");
+
+// require('dotenv').config()
+// console.log(process.env)
+
+const crx = require('crx');
+
+const c = new crx({
+    privateKey: process.env.KEY,
+    codebase: "https://example.com/update.crx"
+})
+
+c.load( path.resolve(__dirname)).then(c => c.pack()).then(cb => {
+    const xml = c.generateUpdateXML()
+    fs.writeFileSync("./reddit-load-images-directly-chrome", cb);
+    fs.writeFileSync("./update.xml", xml);
+}).catch(e => console.error(e))
